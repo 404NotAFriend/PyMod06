e3d2740551ac30f9d95769953329c25b6bc87ff2
404NotAFriend <brunodrcgomes@gmail.com>
2026-06-28 16:43:10 +0100

    fixed flake8 issues on distill 0

--- ft_distillation_0.py
+++ ft_distillation_0.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from alchemy.potions import healing_potion , strength_potion
+from alchemy.potions import healing_potion, strength_potion
 
 print("=== Distillation 0 ===")
 print("Direct access to alchemy/potions.py")
 print("Testing strenght_potion:", strength_potion())
 print("Testing healing_potion:", healing_potion())
-
