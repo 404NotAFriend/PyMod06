437e3d7576f65061cb93f1f09cdbe34f2c1c1bb5
404NotAFriend <brunodrcgomes@gmail.com>
2026-06-29 16:45:35 +0100

    Created new grimoire dir with 4 files in it , dark and light validator and spellbook , also fixed alchemy init and transmutation files to 100%

--- ft_transmutation_1.py
+++ ft_transmutation_1.py
@@ -7,5 +7,3 @@ import alchemy.transmutation
 print("=== Transmutation 1 ===")
 print("Import transmutation module directly")
 print("Testing lead to gold:", alchemy.transmutation.lead_to_gold())
-
-
